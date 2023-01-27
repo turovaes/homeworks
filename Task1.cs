9320535acff3df8fa61baabcaf7ef30ce3ff76c3
turovaes <turovaes@gmail.com>
2023-01-27 21:45:31 +0100

    Changed the file name

---
+++ Task1.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите первое число");
+
+if (!int.TryParse(Console.ReadLine(), out int number1))
+{
+    Console.WriteLine("Вы ввели нецелое число");
+}
+
+Console.WriteLine("Введите второе число");
+
+if (!int.TryParse(Console.ReadLine(), out int number2))
+{
+    Console.WriteLine("Вы ввели нецелое число");
+}
+
+
+if (number1 > number2)
+{
+    Console.WriteLine ("Большее число = " + number1 + ", меньшее число = " + number2 );
+}
+else 
+{
+    Console.WriteLine ("Большее число = " + number2 + ", меньшее число = " + number1);
+}
+
+
+ 
+
+
+
+
