0e90fc04bc77d35e6eba1f97f6c537effe465237
turovaes <turovaes@gmail.com>
2023-01-29 20:56:28 +0100

    Solving of the task3

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите число ");
+
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    int mod = number % 2;
+}
+else Console.WriteLine("Вы ввели нецелое число")
+
+if(mod == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
